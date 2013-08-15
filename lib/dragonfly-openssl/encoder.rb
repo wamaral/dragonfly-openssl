b69fa9b0def7caa9e96bbf3720a597c237e222cf
Wagner Amaral <wamaral@wamaral.org>
2013-08-15 16:53:31 -0300

    adding quotes to key param, and bringing var names up to date

--- lib/dragonfly-openssl/encoder.rb
+++ lib/dragonfly-openssl/encoder.rb
@@ -2,25 +2,25 @@ module Dragonfly
   module OpenSSL
     class Encoder
       include ::Dragonfly::Configurable
-      configurable_attr :keyfile_path, nil
+      configurable_attr :key, nil
 
       def encode(temp_object, format, options = {})
         throw :unable_to_handle unless [:encrypt, :decrypt].include?(format)
-        throw :unable_to_handle if keyfile_path.nil?
+        throw :unable_to_handle unless key
 
         options[:meta] ||= {}
 
         original_filename = temp_object.original_filename
 
-        tempfile = Tempfile.new(original_filename)
-        tempfile.binmode
-        tempfile.close
+        temp_file = Tempfile.new(original_filename)
+        temp_file.binmode
+        temp_file.close
 
         dec = (format == :decrypt) ? '-d ' : ''
 
-        %x{openssl enc -aes-256-cbc #{dec} -in "#{temp_object.path}" -out "#{tempfile.path}" -k "#{keyfile_path}"}
+        %x{openssl enc -aes-256-cbc #{dec} -in "#{temp_object.path}" -out "#{temp_file.path}" -pass pass:"#{key}"}
 
-        content = ::Dragonfly::TempObject.new(File.new(tempfile.path))
+        content = ::Dragonfly::TempObject.new(File.new(temp_file.path))
         meta = {
             name: original_filename,
             encrypted: (format != :decrypt)
